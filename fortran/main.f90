8317fc9fd1be12bae8c9e37c2317aa797f22f4b0
SunkPlane29 <andrezito300@gmail.com>
2024-09-02 18:45:53 -0300

    fix fortran output file extension

--- fortran/main.f90
+++ fortran/main.f90
@@ -21,10 +21,10 @@ program main
     call read_eos("../eos.csv")
     call solve_mrdiagram(n, p0, mrdiagram)
 
-    open(newunit=unit, file="out/mrdiagram.csv")
+    open(newunit=unit, file="out/mrdiagram.dat")
     do i = 1, n
         write(unit, *) mrdiagram(i, 1), mrdiagram(i, 2), mrdiagram(i, 3)
     end do
     close(unit)
-    
+
 end program main
