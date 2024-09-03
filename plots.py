39172abaf238a305886aa1f321bc58bc40fc5469
SunkPlane29 <andrezito300@gmail.com>
2024-09-02 21:26:13 -0300

    add EoS plot

--- plots.py
+++ plots.py
@@ -3,12 +3,22 @@ import scienceplots
 import pandas as pd
 
 def main():
+    eos_df = pd.read_csv("eos.csv", header=None, names=["p", "e"], skipinitialspace=True)
+
     #NOTE: I dont know how to read fortran data files
     fortran_df = pd.read_csv("fortran/out/mrdiagram.dat", sep=" ", header=None, names=["p0", "m", "r", "nan"], skipinitialspace=True)
     python_df = pd.read_csv("python/out/mrdiagram.dat", sep=" ", header=None, names=["p0", "m", "r"], skipinitialspace=True)
     julia_df = pd.read_csv("julia/out/mrdiagram.dat", sep=" ", header=None, names=["p0", "m", "r"], skipinitialspace=True)
 
     plt.style.use("science")
+
+    fig, ax = plt.subplots()
+    ax.plot(eos_df["p"], eos_df["e"], label="EOS", linestyle="-", color="red", linewidth=2)
+    ax.set_xlabel("$P$ [MeV/fm$^3$]")
+    ax.set_ylabel("$\epsilon$ [MeV/fm$^3$]")
+    ax.legend()
+    plt.savefig("out/eos.png", dpi=400)
+
     fig, ax = plt.subplots()
     ax.plot(fortran_df["r"], fortran_df["m"], label="Fortran", linestyle="-", color="grey", linewidth=2)
     ax.plot(julia_df["r"], julia_df["m"], label="Julia", linestyle=(0, (5, 2)), color="green", linewidth=2)
