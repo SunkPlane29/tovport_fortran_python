d0e7a1ad9e4ab221b72014e46df8b48ce6430de2
André da Silva <andrezito300@gmail.com>
2024-11-08 18:23:12 -0300

    run checks

--- julia/main.jl
+++ julia/main.jl
@@ -10,23 +10,45 @@ using DataInterpolations
 import TOV
 
 function main()
-    eoss = Vector{TOV.EoS}()
-    neos = 100
-    for i in 1:neos
-        push!(eoss, TOV.EoS("../eos/out/eos"*string(i)*".csv", ["P", "ϵ"], :linear_interpolation))
-    end
+    # eoss = Vector{TOV.EoS}()
+    # neos = 100
+    # for i in 1:neos
+    #     push!(eoss, TOV.EoS("../eos/out/eos"*string(i)*".csv", ["P", "ϵ"], :linear_interpolation))
+    # end
 
+    # P0i = 5.0
+    # P0f = 800.0
+    # nstars = 100
+    # u = range(log(P0i), log(P0f), length=nstars)
+    # P0 = exp.(u).*TOV.MeVfm3
+
+    # @time Threads.@threads for i in 1:neos
+    #     eos = eoss[i]
+    #     mrdiagram = TOV.solvemrdiagram(P0, P->eos(P), 1.0TOV.SI_TO_LENGTH_UNIT)
+    #     writedat("out/mrdiagram"*string(i)*".dat", mrdiagram[:,1], mrdiagram[:,2], mrdiagram[:,3])
+    # end
+    
+    eos1 = TOV.EoS("../EOSTRS1.3.csv", ["P", "ϵ"], :linear_interpolation)
     P0i = 5.0
-    P0f = 800.0
+    P0f = 480.0
     nstars = 100
     u = range(log(P0i), log(P0f), length=nstars)
     P0 = exp.(u).*TOV.MeVfm3
 
-    @time Threads.@threads for i in 1:neos
-        eos = eoss[i]
-        mrdiagram = TOV.solvemrdiagram(P0, P->eos(P), 1.0TOV.SI_TO_LENGTH_UNIT)
-        writedat("out/mrdiagram"*string(i)*".dat", mrdiagram[:,1], mrdiagram[:,2], mrdiagram[:,3])
-    end
+    push!(eos1.itp.a, last(eos1.itp.a))
+    push!(eos1.itp.b, last(eos1.itp.b))
+
+    mrdiagram = TOV.solvemrdiagram(P0, P->eos1(P), 10.0TOV.SI_TO_LENGTH_UNIT)
+    writedat("out/0mrdiagram.dat", mrdiagram[:,1], mrdiagram[:,2], mrdiagram[:,3])
+    
+    eos2 = TOV.EoS("../EOSMSS1.3.csv", ["P", "ϵ"], :linear_interpolation)
+
+    mrdiagram = TOV.solvemrdiagram(P0, P->eos2(P), 1.0TOV.SI_TO_LENGTH_UNIT)
+    writedat("out/1mrdiagram.dat", mrdiagram[:,1], mrdiagram[:,2], mrdiagram[:,3])
+
+    plot(P0, eos1.(P0), label="EOS TRS1.3", xaxis="Pressure [MeV/fm³]", yaxis="Energy density [MeV/fm³]", title="EOS TRS1.3")
+    plot!(P0, eos2.(P0), label="EOS MSS1.3")
+    gui()
 end
 
 function formatandjoin(v::AbstractVector)::String
--- plots.py
+++ plots.py
@@ -25,10 +25,16 @@ def main():
     fig, ax = plt.subplots()
     # ax.plot(fortran_df["r"], fortran_df["m"], label="Fortran", linestyle="-", color="grey", linewidth=1)
     # ax.plot(python_df["r"], python_df["m"], label="Python", linestyle=(0, (1, 3)), color="blue", linewidth=1)
-    neos = 100
-    for i in range(0, neos):
-        julia_df = pd.read_csv(f"julia/out/mrdiagram{i+1}.dat", sep=" ", header=None, names=["p0", "m", "r"], skipinitialspace=True)
-        ax.plot(julia_df["r"], julia_df["m"], linewidth=1)
+    # neos = 100
+    # for i in range(0, neos):
+    #     julia_df = pd.read_csv(f"julia/out/mrdiagram{i+1}.dat", sep=" ", header=None, names=["p0", "m", "r"], skipinitialspace=True)
+    #     ax.plot(julia_df["r"], julia_df["m"], linewidth=1)
+
+    trsdf = pd.read_csv("julia/out/0mrdiagram.dat", sep=" ", header=None, names=["p0", "m", "r"], skipinitialspace=True)
+    mssdf = pd.read_csv("julia/out/1mrdiagram.dat", sep=" ", header=None, names=["p0", "m", "r"], skipinitialspace=True)
+
+    ax.plot(trsdf["r"], trsdf["m"], label="TRS", linestyle="-", color="blue", linewidth=1)
+    # ax.plot(mssdf["r"], mssdf["m"], label="MSS", linestyle="--", color="red", linewidth=1)
     
     ax.set_xlabel("$R$ [km]")
     ax.set_ylabel("$M$ [M$_\odot$]")
